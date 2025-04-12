8c14c55b3b6cebce1d03c565bacd9ee120c86e6e
Ayush Alag <aalag@stanford.edu>
2025-04-12 12:33:04 -0700

    cross entropy

--- cs336_basics/trainer.py
+++ cs336_basics/trainer.py
@@ -3,12 +3,14 @@ from torch import Tensor
 
 
 def cross_entropy_loss(predictions: Tensor, targets: Tensor) -> Tensor:
-    # predictions: (batch_size, sequence_length, vocab_size)
-    # targets: (batch_size, sequence_length)
+    # predictions: (..., vocab_size)
+    # targets: (..., )
     # log(sum(exp(o - max))) - (o - max)
-    o_is = predictions[targets]
-    o_is_max = o_is.max(dim=-1, keepdim=True)[0]
-    stable_o_is = o_is - o_is_max
-    exp_stable_o_is = torch.exp(stable_o_is)
-    log_sum = torch.log(exp_stable_o_is.sum(dim=-1, keepdim=True))
-    return log_sum - o_is_max[torch.arange(predictions.shape[0]), targets]
+    stable_predictions = predictions - predictions.max(dim=-1, keepdim=True)[0]
+    exp_stable_predictions = torch.exp(stable_predictions)
+    log_sum = torch.log(exp_stable_predictions.sum(dim=-1, keepdim=True))
+
+    negative_log_likelihood = log_sum - stable_predictions
+
+    target_log_likelihood = negative_log_likelihood.gather(dim=-1, index=targets.unsqueeze(-1)).squeeze(-1)
+    return target_log_likelihood.mean()
